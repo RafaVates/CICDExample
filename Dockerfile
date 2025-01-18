eceea54045c045b518272bb4aa8ddd61c7006064
EPAM\rafael_toutain <rafael_toutain@epam.com>
2025-01-18 15:31:19 -0300

    variables de entorno

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:alpine
 WORKDIR /app
+ENV VITE_NOMBRE="Rafa"
+ARG VITE_EDAD
+ENV VITE_EDAD=$VITE_EDAD
 COPY package.json .
 RUN npm install
 COPY . .
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [nombre, setNombre]=useState('')
+
+  useEffect(() => {
+    console.log('useEffect')
+    setNombre(import.meta.env.VITE_NOMBRE)
+    console.log(import.meta.env.VITE_NOMBRE)
+    console.log(import.meta.env.VITE_APELLIDO)
+    console.log(import.meta.env.VITE_EDAD)
+    console.log(import.meta.env.VITE_CARGO)
+  },[])
 
   return (
     <>
@@ -18,6 +28,7 @@ function App() {
       </div>
       <h1>Mi version de prueba para crear un pipeline/actions en gitlab/github</h1>
       <p>Verificando mi primer CI</p>
+      <p>Nombre: {nombre} - {import.meta.env.VITE_CARGO}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
